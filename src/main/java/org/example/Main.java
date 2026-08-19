c57a0bdacb0a2361d0b3916fd7e6aca8db937f81
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-08-18 23:12:38 -0400

    Adding some construction practices

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,9 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.isRunning = true;
-        car.stop();
-        car.start();
-        System.out.println(car.make);
-        System.out.println(car.model);
-        System.out.println(car.price);
-        System.out.println(car.isRunning);
+       Student student = new Student("Kaleab",28,3.5);
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.gpa);
     }
 }
