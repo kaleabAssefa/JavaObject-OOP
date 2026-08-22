cd708e879b2b591cc3248fc46d59e324d3fabad6
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-08-21 23:43:27 -0400

    Adding methods to car class

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -11,4 +11,25 @@ public class Student{
         this.gpa = gpa;
 
     }
+    // Object properties
+    String brand;
+    int year;
+
+    // Method
+    void displayCarInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Year: " + year);
+    }
+
+    public static void main(String[] args) {
+
+        // Creating an object
+        Car myCar = new Car();
+
+        // Assigning values
+        myCar.brand = "Honda";
+        myCar.year = 2024;
+
+        // Calling the method
+        myCar.displayCarInfo();
 }
