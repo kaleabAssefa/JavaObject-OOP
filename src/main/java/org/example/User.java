5022e41eca9067ede5e75c2721011d5b4c9b8a78
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-08-23 22:11:01 -0400

    Adding constructor practices

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class User {
+    String userName;
+    String email;
+    int age;
+
+    User(String userName){
+        this.userName =userName;
+        this.email="Not Provided";
+        this.age = 0;
+    }
+}
